d9497be1ccc2bd7bc2bb8f3db16cb4950e2079d5
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2016-10-23 21:18:33 +0100

    two animations and console logging

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Blinkt = require('Blinktjs');
+const Blinkt = require('blinktjs');
 const mqtt = require('mqtt');
 
 const blinkt = new Blinkt({defaultBrightness: 0.1, colorFormat: 'HSV'});
 const MQTT_BROKER_URL = process.env.MQTT_BROKER_URL || 'mqtt://127.0.0.1';
 
-let timer = null, i = 0;
+let timer = null;
 
 // clear any old lights on startup
 blinkt.off();
@@ -13,9 +13,12 @@ blinkt.off();
 const client = mqtt.connect(MQTT_BROKER_URL);
 
 client.subscribe(['blinkt']);
+console.info('connected and subscribed to "blinkt" channel');
 
-client.on('message', msg => {
+client.on('message', (token, msg) => {
 	let payload = JSON.parse(msg);
+
+	console.info(`Received message on channel ${token}: ${msg.command}, ${msg.animation}, ${msg.duration}`);
 
 	if (!payload.command) { return; }
 
@@ -33,21 +36,34 @@ client.on('message', msg => {
 	}
 });
 
-
 function playAnimation(animation, duration) {
-	// ignores animation for now, as there is only one
-	timer = setInterval(() => {
-		i++;
-		for (let p = 0; p < 8; p++) {
-			blinkt.setPixel(p, ((p * 10) + (i * 5)) % 360, 100, 100);
-		}
-		blinkt.draw();
-	}, 50);
+	let i = 0;
+
+	switch(animation) {
+		case "rainbow":
+			timer = setInterval(() => {
+				i++;
+				for (let p = 0; p < 8; p++) {
+					blinkt.setPixel(p, ((p * 10) + (i * 5)) % 360, 100, 100);
+				}
+				blinkt.draw();
+			}, 50);
+			break;
+
+		case "rotate":
+			blinkt.setAll(255, 0, 0);
+			blinkt.setPixel(3, 200, 255, 0);
+			blinkt.draw();
+			timer = setInterval(() => {
+				blinkt.rotateLeft();
+				blinkt.draw();
+			}, 150);
+			break;
+	}
 
 	setTimeout(() => {
 		stopAnimation();
 	}, duration || 5000);
-
 }
 
 function stopAnimation() {
